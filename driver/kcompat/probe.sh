62e2f6b1605381206399c032fb3bc27ab5a3209b
Vlad-Gabriel Serbu <Vlad-Gabriel.Serbu@amd.com>
2026-05-20 13:34:09 +0100

    Missing license headers
    Signed-off-by: Vlad-Gabriel Serbu <Vlad-Gabriel.Serbu@amd.com>

--- driver/kcompat/probe.sh
+++ driver/kcompat/probe.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+#/**
+# * Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+# * This program is free software; you can redistribute it and/or modify it under the terms of the
+# * GNU General Public License as published by the Free Software Foundation; version 2.
+# *
+# * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# * General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License along with this program; if
+# * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# * 02110-1301, USA.
+# */
+
 # Probe the kernel build at $1 for SLASH API compatibility features.
 #
 # Each *.c file in this directory is built as a tiny standalone module
