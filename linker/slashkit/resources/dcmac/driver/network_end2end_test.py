b52c512d322a737342ca8d3b708a35324bb27773
Mario Ruiz <11815099+mariodruiz@users.noreply.github.com>
2026-07-14 14:24:01 +0100

    More new lines

--- linker/slashkit/resources/dcmac/driver/network_end2end_test.py
+++ linker/slashkit/resources/dcmac/driver/network_end2end_test.py
@@ -99,11 +99,11 @@ def main(args):
         nl1.get_debug_stats(True)
         print('\n')
 
-    print(f'{dcmac0.tx_stats(verbose=1)=}')
+    print(f'{dcmac0.tx_stats(verbose=1)=}\n')
     print(f'{dcmac0.rx_stats(verbose=1)=}\n\n')
 
-    print(f'{dcmac1.tx_stats(verbose=1)=}')
-    print(f'{dcmac1.rx_stats(verbose=1)=}')
+    print(f'{dcmac1.tx_stats(verbose=1)=}\n')
+    print(f'{dcmac1.rx_stats(verbose=1)=}\n\n')
 
     if args.udp:
         print(f'{nl0.get_freq=}')
